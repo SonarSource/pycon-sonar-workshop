1ae4e8f44982aa34b25ad0bad2732393772246a9
Guillaume Dequenne <guillaume.dequenne@sonarsource.com>
2022-04-19 11:40:06 +0200

    Add awesome new feature

--- pokedex/helper.py
+++ pokedex/helper.py
@@ -1,12 +1,18 @@
 import re
 import sqlite3
 
+from pokedex.utils import is_pikachu
+
 
 class ConnectionWrapper:
     """A simple connection wrapper class"""
 
     def __init__(self, db_path):
         self.__conn = sqlite3.connect(db_path)
+
+    def get_single_pokemon(self, pokemon_id):
+        statement = f"SELECT * FROM POKEDEX WHERE id = '{pokemon_id}'"
+        return self.__conn.execute(statement).fetchone()
 
     def get_all_pokemons(self):
         statement = "SELECT * FROM POKEDEX"
@@ -36,3 +42,14 @@ def register_subscriber(wrapper: ConnectionWrapper, email):
         ValueError("Invalid email!")
     wrapper.register_subscriber(email)
     pass
+
+
+def fetch_pokemon(wrapper: ConnectionWrapper, pokemon_id: str):
+    result = wrapper.get_single_pokemon(pokemon_id)
+    if result is None:
+        raise "Pokemon not found"
+    if pokemon_id == 25:
+        if is_pikachu(result):
+            # Team Rocket is trying to steal Pikachu (#25)!
+            result = ("", "We stole Pikachu!", "", "")
+    return result
