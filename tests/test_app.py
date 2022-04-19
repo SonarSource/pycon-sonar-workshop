8f824600a04593a463f3cb02e19285c7ff026405
Guillaume Dequenne <guillaume.dequenne@sonarsource.com>
2022-04-19 11:07:44 +0200

    Add newsletter subscription form

--- tests/test_app.py
+++ tests/test_app.py
@@ -8,3 +8,9 @@ def test_index(client):
     response = client.get("/")
     assert response.status_code == 200
     assert b"<h1>Pokedex</h1>\n" in response.data
+
+
+def test_subscribe(client):
+    helper.register_subscriber = Mock()
+    response = client.post("/subscribe", data={"email": "blah@example.com"})
+    assert response.status_code == 302
